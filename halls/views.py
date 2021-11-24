520a0833f96aaee6e354b8fedbd48b7741a4fa1b
YuhangXiong <yxiong4@ualberta.ca>
2021-11-23 23:03:34 -0700

    added seed data and finish the rest of the functions

--- halls/views.py
+++ halls/views.py
@@ -16,7 +16,8 @@ youtube_api_key = "AIzaSyDpxFgdNDLaSvuwtj4j1QBz9hQNSg80sDE"
 
 def home(request):
     recent_halls = Hall.objects.all().order_by('-id')[:3]
-    popular_halls = [Hall.objects.get(pk = 7),Hall.objects.get(pk = 2)] 
+    popular_halls = [Hall.objects.get(pk = 1),Hall.objects.get(pk = 2),Hall.objects.get(pk = 3)] 
+    
     return render(request, 'halls/home.html',{'recent_halls':recent_halls,'popular_halls':popular_halls})
 
 
@@ -27,7 +28,7 @@ def dashboard(request):
 
 class SignUp(generic.CreateView):
     form_class = UserCreationForm
-    success_url = reverse_lazy('home')
+    success_url = reverse_lazy('dashboard')
     template_name = 'registration/signup.html'
 
     def form_valid(self, form):
